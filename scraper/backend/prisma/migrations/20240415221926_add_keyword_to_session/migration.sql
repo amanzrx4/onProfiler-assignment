ebaf11a7fa26e85ec2545511a9b97c0ddb179ac3
aman sagar <amansagar262@gmail.com>
2024-04-16 03:53:58 +0530

    chore: add keyword to db

---
+++ scraper/backend/prisma/migrations/20240415221926_add_keyword_to_session/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "ScrapSession" ADD COLUMN     "keyword" TEXT NOT NULL DEFAULT '';
--- scraper/frontend/src/components/SessionDetail.tsx
+++ scraper/frontend/src/components/SessionDetail.tsx
@@ -162,6 +162,12 @@ const SessionDetail = ({ sessionId }: { sessionId: string }) => {
 
       {/*  */}
 
+      <div>
+        <h1 className="text-xl text-left my-4">
+          Results for keyword: {profiles.data.data.keyword}
+        </h1>
+      </div>
+
       <div className="relative overflow-x-auto">
         <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
